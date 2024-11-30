1798c60d0dcff3fc4bbcd082a70c9b2cb1ed3e13
Carlos Boccchio <71039570+kaly-montesdeoca@users.noreply.github.com>
2024-11-30 19:00:55 -0300

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {},
+  })
+
+// End Vuetify
+
+//pinia
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+// end pinia
+createApp(App).use(vuetify).use(pinia).mount("#app");
